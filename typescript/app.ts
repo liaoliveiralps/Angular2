26a3d5becfdf5caabc8c6a638c929a9b2f0aa3f8
Liandra de Oliveira Lopes <77747449+liaoliveiralps@users.noreply.github.com>
2021-07-20 16:08:49 -0300

    Add files via upload

---
+++ typescript/app.ts
@@ -0,0 +1,5 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message);
+
+let episode: number = 4
+console.log("This is episode " + episode)
---
+++ typescript/dist/app.js
@@ -0,0 +1,2 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
